8873c4322f028152ad6a62786505884ecbf7fb82
Sohamkale123 <kalesoham82@gmail.com>
2025-08-23 11:32:33 +0530

    views & urls

--- organic/views.py
+++ organic/views.py
@@ -1,1 +1,9 @@
 # I have created this file soham kale
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello")
+
+def about(request):
+    return HttpResponse("This is about page")
